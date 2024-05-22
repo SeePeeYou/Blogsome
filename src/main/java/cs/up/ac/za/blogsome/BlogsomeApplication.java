8f996c05e38277037cf9a0da0d7f292b72909344
Tshego Manthata <tshego.manthata@infocarehealth.com>
2024-05-22 11:32:08 +0200

    springboot project setup

---
+++ src/main/java/cs/up/ac/za/blogsome/BlogsomeApplication.java
@@ -0,0 +1,13 @@
+package cs.up.ac.za.blogsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsomeApplication.class, args);
+	}
+
+}
